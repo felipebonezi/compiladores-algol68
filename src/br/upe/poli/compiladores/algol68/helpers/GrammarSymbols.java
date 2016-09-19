7bbdfb04ff6b9b39a1500788e756c873dd6ac1d2
felipebonezi <felipebonezi@gmail.com>
2016-09-18 21:57:45 -0300

    - Configuração inicial do projeto com as classes base do projeto.

--- src/br/upe/poli/compiladores/algol68/helpers/GrammarSymbols.java
+++ src/br/upe/poli/compiladores/algol68/helpers/GrammarSymbols.java
@@ -6,5 +6,13 @@ public class GrammarSymbols {
 	// Language terminals (starts from 0)
 	public static final int ID = 0;
 	public static final int EOT = 1000;
-	
+
+    public static final int SEMICOLON = 1;
+    public static final int OP_BASIC = 2;
+    public static final int OP_FACTOR = 3;
+    public static final int COMMA = 4;
+    public static final int EQUALS = 5;
+    public static final int TWO_DOTS = 6;
+    public static final int R_PAR = 7;
+    public static final int L_PAR = 8;
 }
